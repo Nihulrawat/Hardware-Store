928b703241aee8a86e463d97306d1e86bbf5fb5e
Nihul Rawat <nihul.rawat@gmail.com>
2015-08-18 16:28:29 +0530

    modified the MainApp.java to greet

--- Demo1/src/mainpack/MainApp.java
+++ Demo1/src/mainpack/MainApp.java
@@ -3,7 +3,7 @@ package mainpack;
 public class MainApp {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	System.out.println("hello sapient");	// TODO Auto-generated method stub
 
 	}
 
